51ac28357ef2b3b484b06f3e32475ef63d80cd3e
Manh-Hao Pham <manhhaoo.do@gmail.com>
2025-02-01 15:24:31 +0100

    Fix NullPointerException when adding new card using Local Audio

--- app/src/main/java/com/kamwithk/ankiconnectandroid/routing/LocalAudioAPIRouting.java
+++ app/src/main/java/com/kamwithk/ankiconnectandroid/routing/LocalAudioAPIRouting.java
@@ -24,7 +24,9 @@ import com.kamwithk.ankiconnectandroid.routing.localaudiosource.Shinmeikai8Audio
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -223,7 +225,16 @@ public class LocalAudioAPIRouting {
 
         EntriesDatabase db = getDB();
         AudioFileEntryDao audioFileEntryDao = db.audioFileEntryDao();
-        byte[] data = audioFileEntryDao.getData(path, source);
+
+        String pathDecoded;
+        try {
+            pathDecoded = URLDecoder.decode(path, "UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            pathDecoded = path;
+        }
+
+        byte[] data = audioFileEntryDao.getData(pathDecoded, source);
 
         // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
         String mimeType = null;
